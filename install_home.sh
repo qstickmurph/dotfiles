230c7abebbfc5b781da4e85755527482ee14d03e
Nragis <Quinn_murphey@me.com>
2023-09-26 20:43:49 -0600

    Added xinit and nvidia-settings

--- install_home.sh
+++ install_home.sh
@@ -5,4 +5,10 @@ ln -s ./home/bash/.bash_history ~/.bash_history
 ln -s ./home/bash/.bash_login ~/.bash_login
 ln -s ./home/bash/.bash_logout ~/.bash_logout
 
+# Xinit
+ln -s ./home/.xinit ~/.xinit
 
+# Nvidia Settings
+ln -s ./home/.nvidia-settings-rc ~/.nvidia-settings-rc
+
+# 
