354f6e5fd967d696ec787af0dd1c63fd7a6f58e1
qstickmurph <quinn@stickmurph.com>
2025-10-08 15:43:18 -0600

    Updated neovim plugins

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -1,3 +1,7 @@
 return {
     "neovim/nvim-lspconfig",
+    config = function ()
+      vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+    end
 }
