67fc919e76c02c72f502e5199077931ae3e42a94
qstickmurph <quinn@stickmurph.com>
2025-10-17 17:31:13 -0600

    Added tmux configuration

--- config/nvim/lua/config/remap.lua
+++ config/nvim/lua/config/remap.lua
@@ -1,11 +1,15 @@
+-- Open file explorer
 vim.keymap.set("n", "<leader>pd", vim.cmd.Ex)
 
+-- Change escape commands
 vim.keymap.set("i", "jh", "<esc>")
 vim.keymap.set("i", "jh", "<C-C>")
 
+-- Move lines in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Recenter window on page down
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
@@ -23,7 +27,12 @@ vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>cprev<CR>zz")
 
+-- Open error
 vim.keymap.set("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>");
 
 vim.keymap.set("n", "<Leader>b", ":bp<CR>")
 vim.keymap.set("n", "<Leader>f", ":bn<CR>")
+
+-- Window commands
+vim.keymap.set("n", "<C-w>-", ":split<CR>")
+vim.keymap.set("n", "<C-w>\\", ":vsplit<CR>")
